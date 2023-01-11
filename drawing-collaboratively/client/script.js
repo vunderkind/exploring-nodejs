1a990f68e91eb18892b543bad393fc3b275e00c6
Justin Irabor <justin.irabor@gmail.com>
2023-01-11 23:33:12 +0100

    completed canvas

--- drawing-collaboratively/client/script.js
+++ drawing-collaboratively/client/script.js
@@ -5,7 +5,8 @@ let canvas = document.getElementById('canvas');
 let context = canvas.getContext('2d');
 let pointerContainer = document.getElementById("pointers");
 let pointer = document.createElement("div");pointer.setAttribute("class", "pointer");
-let drawing = false;let clients = {};
+let drawing = false;
+let clients = {};
 let pointers = {};
 function drawLine(fromx, fromy, tox, toy) {
     context.moveTo(fromx, fromy);
@@ -31,15 +32,39 @@ canvas.onmouseup =
                     }
                     if(drawing) {
                         drawLine(prev.x, prev.y, e.pageX, e.pageY);
-                        rev.x = e.pageX;        prev.y = e.pageY;
+                        prev.x = e.pageX;
+                        prev.y = e.pageY;
                     }
                     break;
                 case "mousedown":
                     drawing = true;
-                    prev.x = e.page.X;
-                    prev.y = e.page.Y;
+                    prev.x = e.pageX;
+                    prev.y = e.pageY;
                     break;
                 default:
                     break;
             };
         };
+
+socket.on('moving', data => {
+    if(!clients.hasOwnProperty(data.id)) {
+        pointers[data.id] = pointerContainer.appendChild(pointer.cloneNode());
+    };
+
+    pointers[data.id].style.left = data.x + 'px';
+    pointers[data.id].style.top = data.y + 'px';
+
+    if(data.drawing && clients[data.id]) {
+        drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
+    }
+
+    clients[data.id] = data;
+    clients[data.id].updated = now();
+});
+
+socket.on('clientdisconnect', id => {
+    delete clients[id];
+    if (pointers[id]) {
+        pointers[id].parentNode.removeChild(pointers[id]);
+            }
+})
--- drawing-collaboratively/index.js
+++ drawing-collaboratively/index.js
@@ -1,5 +1,15 @@
-const io = require('socket');
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
 
+app.use(express.static('client'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
 io.sockets.on('connection', socket => {
     let id = socket.id;
 
@@ -12,3 +22,8 @@ io.sockets.on('connection', socket => {
         socket.broadcast.emit('clientdisconnect', id);
     });
 })
+
+server.listen(3000, () => {
+    console.log('listening on *:3000');
+  });
+  
